12e76bd1c22f223d27fa70555b0fff40c0846aac
williamsb <williamsb@tutor.com>
2015-01-06 12:10:42 -0500

    new template; add callbacks; updated viewport defaults; updated documentation

--- README.md
+++ README.md
@@ -1,23 +1,41 @@
 # yams.viewport.js
-###### a jQuery plugin that adds viewport classes to the body tag
+# a jQuery plugin that adds viewport classes to the body tag
 
----
+## Summary
 
-### Requires
+Add one of the following classes to the `<body>` element
+
+* `viewport-xs`
+* `viewport-sm`
+* `viewport-md`
+* `viewport-lg`
+
+## Requires
 
 * [jQuery](#http://jQuery.com)
 
-### Usage
+## Usage
 
 ```
-<script type="text/javascript" src="yams.viewport.js">
-
+<script type="text/javascript" src="yams.viewport.js"></script>
+<script type="text/javascript">
+    // add callbacks before document ready
+    $.yams.viewport.addCallback(funcion(viewport) {
+		// viewport object schema
+		// {
+		//     name: name of currently set viewport
+		//     width: current window width
+		//     height: current window height
+		// }
+		console.log('viewport is now ', viewport)
+    })
+</script> 
 ```
 
-#### Notes
+## Notes
 
-* Uses Bootstrap viewport sizes and nomenclature
+* By default, uses Bootstrap 3.2.x viewport sizes and nomenclature
  * `.viewport-xs` (default)
- * `.viewport-sm` (width >= 750)
- * `.viewport-md` (width >= 970)
- * `.viewport-lg` (width >= 1170)
+ * `.viewport-sm` (width >= 768)
+ * `.viewport-md` (width >= 992)
+ * `.viewport-lg` (width >= 1200)
--- yams.viewport.js
+++ yams.viewport.js
@@ -1,58 +1,103 @@
 ﻿
+/* ========================================================================
+* yams.viewport.js
+* version:	1.1
+* author:	Brad Williams <brad.lee.williams@gmail.com>
+* ======================================================================== */
+
 if (!$.yams) { $.yams = new Object(); }
-if (!$.yams.viewport) { $.yams.viewport = new Object(); }
 
-(function ($, $vp) {
+$.yams.viewport = (function () {
+    //private
 
-	$vp.init = function () {
-        $vp.update();
-		$(window).on('resize.yams.viewport', function () {
-            $vp.update();
-        });
+    var DEFAULTS = {
+        // container sizes from Bootstratp 3.2.0
+        sm: 768, // viewport-sm
+        md: 992, // viewport-md
+        lg: 1200, // viewport-lg
+        enableResizeTracking: true,
+    }
+    var _callbacks = []
+    var _lastViewport = {}
+
+    var _getViewport = function () {
+        var viewport = {}
+
+        // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
+        if (typeof window.innerWidth != 'undefined') {
+            viewport.width = window.innerWidth,
+			viewport.height = window.innerHeight
+        } else if (typeof document.documentElement != 'undefined' && typeof document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0) {
+            // IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+            viewport.width = document.documentElement.clientWidth,
+			viewport.height = document.documentElement.clientHeight
+        } else {
+            // older versions of IE
+            viewport.width = document.getElementsByTagName('body')[0].clientWidth,
+			viewport.height = document.getElementsByTagName('body')[0].clientHeight
+        }
+
+        // set name
+        if (viewport.width >= DEFAULTS.lg)
+            viewport.name = 'lg'
+        else if (viewport.width >= DEFAULTS.md)
+            viewport.name = 'md'
+        else if (viewport.width >= DEFAULTS.sm)
+            viewport.name = 'sm'
+        else // default: container-xs
+            viewport.name = 'xs'
+
+        return viewport
+    }
+    var _update = function () {
+        var viewport = _getViewport();
+
+        if (viewport.name) {
+            if (!_lastViewport
+				|| _lastViewport.name != viewport.name) {
+
+                // add class
+                $('body')
+                    .removeClass('viewport-xs viewport-sm viewport-md viewport-lg')
+                    .addClass('viewport-' + viewport.name)
+
+                // run callback
+                if (DEFAULTS.enableResizeTracking) {
+                    for (var i = 0; i < _callbacks.length; i++) {
+                        _callbacks[i](viewport)
+                    }
+                }
+            }
+
+            // set this even if name doesn't change because width/height might
+            _lastViewport = viewport
+        }
     }
 
-    $vp.update = function () {
-    	var viewport = this._getViewport();
-    	var viewportModifier = 'unknown';
-
-    	// use container sizes from Bootstratp 3.1.1
-    	if (viewport.width >= 1170) { // viewport-lg
-    		viewportModifier = 'lg';
-    	} else if (viewport.width >= 970) { // viewport-md
-    		viewportModifier = 'md';
-    	} else if (viewport.width >= 750) { // viewport-sm
-    		viewportModifier = 'sm';
-    	} else { // container-xs
-    		viewportModifier = 'xs';
-    	}
-
-    	$('body').removeClass('viewport-xs viewport-sm viewport-md viewport-lg');
-    	$('body').addClass('viewport-' + viewportModifier);
+    //public
+    return {
+        addCallback: function (callback) {
+            _callbacks.push(callback)
+        },
+        init: function (options) {
+            $.extend(DEFAULTS, options)
+            // initial update
+            $(function () {
+                _update()
+            })
+            // update on resize window
+            if (DEFAULTS.enableResizeTracking) {
+                $(window).on('resize.yams.viewport', function () {
+                    _update()
+                })
+            }
+        },
+        getViewport: function () {
+            return _lastViewport
+        },
     };
+})();
 
-    $vp._getViewport = function () {
-    	var viewPortWidth;
-    	var viewPortHeight;
-
-    	// the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
-    	if (typeof window.innerWidth != 'undefined') {
-    		viewPortWidth = window.innerWidth,
-			viewPortHeight = window.innerHeight
-    	} else if (typeof document.documentElement != 'undefined' && typeof document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0) {
-    		// IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
-    		viewPortWidth = document.documentElement.clientWidth,
-			viewPortHeight = document.documentElement.clientHeight
-    	} else {
-    		// older versions of IE
-    		viewPortWidth = document.getElementsByTagName('body')[0].clientWidth,
-			viewPortHeight = document.getElementsByTagName('body')[0].clientHeight
-    	}
-    	return {
-    		width: viewPortWidth,
-    		height: viewPortHeight
-    	};
-    }
-
-})(jQuery, $.yams.viewport);
-
-$.yams.viewport.init();
+$(function () {
+    $.yams.viewport.init()
+})
